0b94524c461a55b35f0dffed7fab53a72457dcde
MalickNdoye <malickndoye90@hotmail.fr>
2020-12-14 16:02:34 +0100

    matrice de contingence

--- main.R
+++ main.R
@@ -4,10 +4,12 @@
 # Created on: 24/11/2020
 
 library("FactoMineR")
+library("ggplot2")
 library("factoextra")
+library("gplots")
 
 dataset <- read.csv(file="./archive/data.csv", sep=";")
-x <- dataset[1:500,c(1,3,5,7,11,12,13)]
+x <- dataset[1:100,c(3,5,7,11,12,13)]
 acp_fonction <- function(x) {
   pca = PCA(x,scale.unit = TRUE, ncp=5,graph=TRUE)
   pca
@@ -15,3 +17,10 @@ acp_fonction <- function(x) {
 
 acp_fonction(x)
 #fviz_contrib(acp1, choice = "var", axes = 1, top = 10)
+
+
+# 1. convertir les données en tant que table
+dt <- as.table(as.matrix (x))
+# 2. Graphique
+mosaic-plot(t (dt), main = "dataset", xlab = "", ylab = "",
+            label = FALSE, show.margins = FALSE)
